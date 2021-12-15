43b82158f114ddae07092545549637f7b757dd96
Alexandra <alexandraberdysheva@gmail.com>
2021-12-15 17:51:16 +0300

    Finish desktop HTML and CSS layout

--- app/js/index.js
+++ app/js/index.js
@@ -13,6 +13,20 @@ $(function () {
         autoplay: true,
     });
 
+    $('.contact-slider').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 10,
+        slidesToScroll: 10,
+    });
+
+    $('.article-slider__box').slick({
+        prevArrow:
+            '<button type="button" class="article-slider__arrow article-slider__arrow-left"><img src="./images/arrow-slide-left.svg" alt="arrow left"></button>',
+        nextArrow:
+            '<button type="button" class="article-slider__arrow article-slider__arrow-right"><img src="./images/arrow-slide-right.svg" alt="arrow right"></button>',
+    });
+
     var mixer = mixitup('.gallery__inner', {
         load: {
             filter: '.bedroom',
